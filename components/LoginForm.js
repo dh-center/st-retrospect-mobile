f6c0642008c0ed186e6d514179d19470f7fb24fc
lersatromtsova <v.stromtsova@yandex.ru>
2019-10-24 01:38:02 +0300

    Login form improved

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -1,2 +1,31 @@
 import React, { Component } from 'react';
+import {Container, Content, Form, Item, Input, Button, Header} from 'native-base';
+
+
+export default class SignUpForm extends Component {
+    static navigationOptions = {
+        title: 'Log In',
+    };
+    render() {
+        return (
+            <Container>
+                <Header/>
+                <Content>
+                    <Form>
+                        <Item>
+                            <Input placeholder="Username" />
+                        </Item>
+                        <Item>
+                            <Input placeholder="Password" />
+                        </Item>
+                        <Button
+                            title="Log In"
+                            onPress={() => this.props.navigation.navigate('RoutesList')}
+                        />
+                    </Form>
+                </Content>
+            </Container>
+        );
+    }
+}
 
