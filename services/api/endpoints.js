90fce88eb331d4ac977fa8be7280eb9a514ae742
lersatromtsova <v.stromtsova@yandex.ru>
2019-11-04 22:17:24 +0300

    Put API services into separate module

---
+++ services/api/endpoints.js
@@ -0,0 +1,2 @@
+export const signUpUrl = 'https://api.st-retrospect.dh-center.ru/sign-up';
+export const logInUrl  = 'https://api.st-retrospect.dh-center.ru/login';
---
+++ services/api/requests.js
@@ -0,0 +1,47 @@
+import { signUpUrl, logInUrl } from './endpoints';
+import {store} from '../../data/users/store';
+import {SAVE_AUTH_TOKEN} from '../../data/users/action_types';
+
+export function sendSignUpRequest(username, password) {
+    fetch(
+        signUpUrl,
+        {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                username: username,
+                password: password,
+            }),
+        }
+    )
+        .then((response) => {
+            if (response.status == 201) {
+                console.log('OK');
+            }
+            else {
+                console.log(response.status);
+            }
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+}
+
+export function sendLogInRequest(username, password) {
+    const encodedUsername = encodeURIComponent(username);
+    const encodedPassword = encodeURIComponent(password);
+    const parmsUrl = logInUrl + `?username=${encodedUsername}&password=${encodedPassword}`;
+
+    fetch(parmsUrl)
+        .then((response) => response.json())
+        .then((responseData) => {
+            store.dispatch({type: SAVE_AUTH_TOKEN, authToken: responseData.data.accessToken});
+            console.log(store.getState());
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+}
