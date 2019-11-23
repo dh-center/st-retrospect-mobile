f9850ef0290fb94a1f2221d66234dc930b9b7dc7
lersatromtsova <v.stromtsova@yandex.ru>
2019-11-23 22:57:42 +0300

    Next point is dynamically updated

--- screens/app/RouteNavigation.js
+++ screens/app/RouteNavigation.js
@@ -4,22 +4,98 @@ import {Body, Button, H2, Icon, ListItem, Right} from 'native-base';
 import {t} from '../../locales/i18n';
 import {GOOGLE_DIRECTIONS_API_KEY} from 'react-native-dotenv';
 import {MapWithMarkers} from './MapWithMarkers';
+import Geolocation from '@react-native-community/geolocation';
+
+const DistanceFilter = 3;
+const LatitudeChangeDelta = 0.0001;
+const LongitudeChangeDelta = 0.0001;
 
 export default class RouteNavigation extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            nextLocation: this.props.navigation.getParam('locations')[0],
+            nextLocationIndex: 0,
+            locations: this.props.navigation.getParam('locations'),
+            latitude: null,
+            longitude: null,
         };
+        this.findCoordinates();
     }
 
-    render() {
-        const locations = this.props.navigation.getParam('locations');
+    findCoordinates = () => {
+        Geolocation.getCurrentPosition(
+            position => {
+                this.setState({
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude,
+                });
+            },
+            error => console.log(error.message),
+            {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000},
+        );
+    };
 
+    getNextLocation() {
+        return this.state.locations[this.state.nextLocationIndex];
+    }
+
+    finishRoute = () => {
+        this.props.navigation.navigate('RouteFinish', {
+            routeId: this.props.navigation.getParam('routeId'),
+        });
+    };
+
+    updateNextLocation = () => {
+        let curLatitudeDelta = Math.abs(
+            this.getNextLocation().latitude - this.state.latitude,
+        );
+        let curLongitudeDelta = Math.abs(
+            this.getNextLocation().longitude - this.state.longitude,
+        );
+
+        if (
+            curLatitudeDelta <= LatitudeChangeDelta &&
+            curLongitudeDelta <= LongitudeChangeDelta
+        ) {
+            const curLocIndex = this.state.nextLocationIndex;
+            if (curLocIndex === this.state.locations.length) {
+                this.finishRoute();
+            } else {
+                this.setState({
+                    nextLocationIndex: curLocIndex + 1,
+                });
+            }
+        }
+    };
+
+    componentDidMount = () => {
+        this.watchID = Geolocation.watchPosition(
+            position => {
+                this.setState({
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude,
+                });
+                this.updateNextLocation();
+            },
+            error => console.log(error),
+            {
+                enableHighAccuracy: true,
+                timeout: 250,
+                maximumAge: 20,
+                distanceFilter: DistanceFilter,
+            },
+        );
+    };
+
+    componentWillUnmount = () => {
+        Geolocation.clearWatch(this.watchID);
+    };
+
+    render() {
         return (
             <View style={styles.container}>
                 <MapWithMarkers
-                    locations={locations}
+                    locations={this.state.locations}
                     deltas={{latitudeDelta: 0.005, longitudeDelta: 0.005}}
                 />
                 <ListItem key={this.props.navigation.getParam('name')}>
@@ -27,18 +103,10 @@ export default class RouteNavigation extends Component {
                         <Text style={{paddingBottom: 10}}>
                             {t('next-stop')}
                         </Text>
-                        <H2>{this.state.nextLocation.name}</H2>
+                        <H2>{this.getNextLocation().name}</H2>
                     </Body>
                     <Right>
-                        <Button
-                            transparent
-                            onPress={() =>
-                                this.props.navigation.navigate('RouteFinish', {
-                                    routeId: this.props.navigation.getParam(
-                                        'routeId',
-                                    ),
-                                })
-                            }>
+                        <Button transparent onPress={() => this.finishRoute()}>
                             <Icon name="md-close-circle" />
                         </Button>
                     </Right>
