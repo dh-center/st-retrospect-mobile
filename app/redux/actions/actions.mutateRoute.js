67fc238051d127be3b610a1a5bcdc4e34e098d69
Valeria Stromtsova <v.stromtsova@yandex.ru>
2019-12-23 01:51:40 +0300

    Cleanup

--- app/redux/actions/actions.mutateRoute.js
+++ app/redux/actions/actions.mutateRoute.js
@@ -6,13 +6,9 @@ import {
     unsaveRoute,
 } from '../../services/api/mutations';
 import {RECEIVE_SAVED_ROUTES} from './actions.savedRoutes';
-import {
-    INVALIDATE_LIKED_ROUTES,
-    RECEIVE_LIKED_ROUTES,
-} from './actions.likedRoutes';
+import {RECEIVE_LIKED_ROUTES} from './actions.likedRoutes';
 import {ApolloClient, HttpLink, InMemoryCache} from 'apollo-boost';
 import {routesUrl} from '../../services/api/endpoints';
-import {likedRoutesQuery, savedRoutesQuery} from '../../services/api/queries';
 
 const client = new ApolloClient({
     link: new HttpLink({
@@ -26,15 +22,14 @@ const client = new ApolloClient({
 });
 
 export function mutateRouteSave(routeId) {
-    console.log(routeId);
     return async () => {
+        let variables = {routeId: routeId};
         const request = await client.mutate({
             mutation: saveRoute,
-            variables: {
-                routeId: routeId,
-            },
+            variables: variables,
         });
         const result = await request;
+        console.log(result.data.saveRoute);
         if (result) {
             store.dispatch({
                 type: RECEIVE_SAVED_ROUTES,
@@ -45,14 +40,11 @@ export function mutateRouteSave(routeId) {
 }
 
 export function mutateRouteUnsave(routeId) {
-    console.log(routeId);
-
     return async () => {
+        let variables = {routeId: routeId};
         const request = await client.mutate({
             mutation: unsaveRoute,
-            variables: {
-                routeId: routeId,
-            },
+            variables: variables,
         });
         const result = await request;
         console.log(result.data.deleteRouteFromSaved);
